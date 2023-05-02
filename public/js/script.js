19c103ee2d6ed8bee86f3c6b22c3dc3a90183bd7
Slava Tsoy <emonsipurewerifaine@yandex.com>
2023-05-03 00:02:33 +0300

    Initialization of firebase project

---
+++ public/js/script.js
@@ -0,0 +1,11 @@
+import '../dist/bundle.js';
+
+// Get a reference to the signupButton element
+const signupButton = document.getElementById('signupButton');
+
+// Add a click event listener to the button
+signupButton.addEventListener('click', ( ) => {
+  // This code will be executed when the button is clicked
+  console.log('Signup button clicked!');
+  // You can add your own code here to handle the button click event
+});
